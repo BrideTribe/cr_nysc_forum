f25b3cfd4fdecb26d64a77dbfc250c589daa9021
Akinnibosun Vicktor <ritymontero@gmail.com>
2018-06-12 09:35:21 +0100

    create home and about routes

--- lib.py
+++ lib.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Victor Abayomi',
+        'title': 'NYSC Forum Post 1',
+        'date_posted': 'June 11, 2018',
+        'content': 'First NYSC Forum post content'
+    },
+    {
+        'author': 'Opeyemi Susan',
+        'title': 'NYSC Forum Post 2',
+        'date_posted': 'June 11, 2018',
+        'content': 'Second NYSC Forum post content'
+    }
+]
 
 @app.route("/")
+@app.route("/home")
 def home():
-    return 'Hello Forum'
+    return render_template('home.html', posts=posts)
 
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True) 
