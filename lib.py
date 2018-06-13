e36f42ead54cd95587ae4c5884d42133bbfcd8be
Akinnibosun Vicktor <ritymontero@gmail.com>
2018-06-13 11:10:22 +0100

    include register and login routes

--- lib.py
+++ lib.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '8639f43b71b224ad036d1047322a6b39'
 posts = [
     {
         'author': 'Victor Abayomi',
@@ -25,6 +28,16 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    retun render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True) 
