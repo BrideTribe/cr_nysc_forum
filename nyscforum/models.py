72341761d0dd010c922a829368b85332759e3525
Akinnibosun Victor A <ritymontero@gmail.com>
2018-10-12 23:58:15 +0100

    include a login manager

--- nyscforum/models.py
+++ nyscforum/models.py
@@ -1,8 +1,13 @@
 from datetime import datetime
-from nyscforum import db
+from nyscforum import db, login_manager
+from flask_login import UserMixin
 
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
     
     __tablename__ = 'users'
 
