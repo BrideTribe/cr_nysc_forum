9d2c0a8b262f4bb09c2307bfd7e816389a167328
Akinnibosun Victor A <ritymontero@gmail.com>
2018-10-27 15:52:01 +0100

    include a line space

--- nyscforum/__init__.py
+++ nyscforum/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8639f43b71b224ad036d1047322a6b39'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///nysc.db'
