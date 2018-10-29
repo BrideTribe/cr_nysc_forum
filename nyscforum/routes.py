c8a092d3248aa018f6cb5fca049944e49f2544ab
Akinnibosun Vicktor <ritymontero@gmail.com>
2018-10-29 16:10:25 +0100

    add state code as part of login requirement and create account route

--- nyscforum/routes.py
+++ nyscforum/routes.py
@@ -64,15 +64,19 @@ def login():
     form = LoginForm()
     
     if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
+        user = User.query.filter_by(email=form.email.data, state_code=form.state_code.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             return redirect(url_for('home'))
         else:
-            flash('Email or Password incorrect', 'danger')
+            flash('Check your email, state code, password and try again', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 @app.route("/logout")
 def logout():
-    login_user()
+    logout_user()
     return redirect(url_for('home'))
+
+@app.route("/account")
+def account():
+    return render_template('account.html', title='Account')
