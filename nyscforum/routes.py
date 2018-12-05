2a257f8be390bed73268171a5431e738a587fa82
Akinnibosun Vicktor <ritymontero@gmail.com>
2018-12-05 17:39:57 +0100

    remove posts list and query it from database

--- nyscforum/routes.py
+++ nyscforum/routes.py
@@ -1,36 +1,16 @@
 import os #to capture picture extension
 import secrets  #this is for the picture function
-from PIL import Image
-from flask import render_template, url_for, flash, redirect,request
+from PIL import Image #python image processing module
+from flask import render_template, url_for, flash, redirect,request, abort
 from nyscforum import app, db, bcrypt
-from nyscforum.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from nyscforum.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from nyscforum.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
-
-posts = [
-    {
-        'author': 'Victor Abayomi',
-        'title': 'NYSC Forum Post 1',
-        'date_posted': 'June 11, 2018',
-        'content': 'First NYSC Forum post content'
-    },
-    {
-        'author': 'Opeyemi Emmanuel',
-        'title': 'NYSC Forum Post 2',
-        'date_posted': 'June 30, 2018',
-        'content': 'Second NYSC Forum post content'
-    },
-    {
-        'author': 'Name Susan',
-        'title': 'NYSC Forum Post 3',
-        'date_posted': 'July 12, 2018',
-        'content': 'Third NYSC Forum post content'
-    }
-]
 
 @app.route("/")
 @app.route("/home")
 def home():
+    posts = Post.query.all()
     return render_template('home.html', posts=posts)
 
 
@@ -141,3 +121,52 @@ def account():
         form.ppa.data =current_user.ppa 
     image_file = url_for('static', filename='images/'+ current_user.image_file)
     return render_template('account.html', title='Account', image_file=image_file, form=form) 
+
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post', 
+                            form=form, legend='New Post')
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post', 
+                            form=form, legend='Update Post')
+
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted', 'success')
+    return redirect(url_for('home'))
