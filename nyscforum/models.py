4dee53b48d94f88bc3c1a3b9bb6eae9a53489c1d
Akinnibosun Vicktor <ritymontero@gmail.com>
2018-12-05 17:37:34 +0100

    change string content and add self.content to __repr__

--- nyscforum/models.py
+++ nyscforum/models.py
@@ -39,10 +39,10 @@ class Post(db.Model):
     __tablename__ = 'posts'
     
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200), nullable=False)
+    title = db.Column(db.String(500), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
     def __repr__(self):
-        return f"Post('{self.title}', '{self.date_posted}')"
+        return f"Post('{self.title}', '{self.date_posted}', '{self.content}')"
